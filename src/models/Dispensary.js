3239898ec47f124636b9e5b8b1c1b05c0059a322
Joao Zsigmond <jzsiggy@gmail.com>
2020-02-04 21:16:11 -0200

    Modifying dispensary model

--- src/models/Dispensary.js
+++ src/models/Dispensary.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dispensarySchema = new Schema({
+  email : {
+    type : String,
+  },
   name : {
     type : String,
   },
